454d6f10e595af80f679b05435ed683fe8a6ca03
Ivan Chernetsky <ivan.chernetsky@gmail.com>
2010-09-28 14:42:29 +0300

    Removing item from list

--- src/basic-ds/list.c
+++ src/basic-ds/list.c
@@ -10,6 +10,7 @@ struct list {
 list *new_item (int);
 list *add_front (list *, list *);
 list *add_end (list *, list *);
+list *remove_item (list *, int);
 void print_backwards (list *);
 
 int main (void) {
@@ -22,6 +23,14 @@ int main (void) {
   head = add_end (head, new_item (5));
 
   print_backwards (head);
+  printf ("\n");
+
+  head = remove_item (head, 3);
+  head = remove_item (head, 1);
+  head = remove_item (head, 5);
+
+  print_backwards (head);
+  printf ("\n");
 
   return EXIT_SUCCESS;
 }
@@ -51,10 +60,27 @@ list *add_end (list *head, list *new) {
   return head;
 }
 
+list *remove_item (list *head, int data) {
+  list *current, *prev = NULL;
+
+  for (current = head; current != NULL; current = current->next) {
+    if (data == current->data) {
+      if (NULL == prev)
+	head = current->next;
+      else
+	prev->next = current->next;
+      free (current);
+      return head;
+    }
+    prev = current;
+  }
+  return head;
+}
+
 void print_backwards (list *elem) {
   if (NULL == elem)
     return;
 
   print_backwards (elem->next);
-  printf ("%x\n", elem->data);
+  printf ("%x ", elem->data);
 }
