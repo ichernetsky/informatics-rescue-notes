a7e61d822f9e144a3a50f300a9b6d68e9bd1fcde
Ivan Chernetsky <ivan.chernetsky@gmail.com>
2010-09-29 18:17:09 +0300

    Priority queue: text and source code

--- src/basic-ds/binheap.c
+++ src/basic-ds/binheap.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 #define HEAP_SIZE 50
 
@@ -17,22 +18,33 @@ inline int right (int);
 void swap (int [], int, int);
 void max_heapify (binheap *, int);
 void build_max_heap (binheap *);
+int heap_extract_max (binheap *);
+int heap_increase_key (binheap *, int, int);
+void heap_insert (binheap *, int);
 void heapsort (binheap *);
 
 int main (void) {
-  binheap heap = { { 9, 1, 8, 2, 7, 3, 6, 4, 5 },
+  binheap heap = { { 9, 1, 11, 13, 7, 3, 15, 17, 5 },
 		   9 };
   int i;
 
   build_max_heap (&heap);
   for (i = 0; i < heap.size; i++)
     printf ("%d ", heap.data[i]);
+  printf ("\n");
 
+  heap_insert (&heap, 8);
+  heap_insert (&heap, 19);
+  (void) heap_extract_max (&heap);
+  (void) heap_increase_key (&heap, 6, 12);
+  for (i = 0; i < heap.size; i++)
+    printf ("%d ", heap.data[i]);
   printf ("\n");
 
   heapsort (&heap);
   for (i = 0; i < heap.size; i++)
     printf ("%d ", heap.data[i]);
+  printf ("\n");
 
   return EXIT_SUCCESS;
 }
@@ -88,6 +100,36 @@ void build_max_heap (binheap *heap) {
     return;
   for (i = toindex (heap->size / 2); i >= 0; i--)
     max_heapify (heap, i);
+}
+
+int heap_maximum (binheap *heap) {
+  return heap->data[0];
+}
+
+int heap_extract_max (binheap *heap) {
+  int max = heap->data[0];
+
+  heap->size -= 1;
+  heap->data[0] = heap->data[heap->size];
+  max_heapify (heap, 0);
+  return max;
+}
+
+int heap_increase_key (binheap *heap, int i, int k) {
+  if (k < heap->data[i])
+    return -1;
+  heap->data[i] = k;
+  while (i > 0 && heap->data[parent (i)] < heap->data[i]) {
+    swap (heap->data, i, parent (i));
+    i = parent (i);
+  }
+  return 0;
+}
+
+void heap_insert (binheap *heap, int k) {
+  heap->data[heap->size] = INT_MIN;
+  heap->size += 1;
+  heap_increase_key (heap, heap->size - 1, k);
 }
 
 void heapsort (binheap *heap) {
