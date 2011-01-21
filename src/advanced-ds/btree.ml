bab11deda71f507c14ef31955af3f067d5f90953
Ivan Chernetsky <ivan.chernetsky@gmail.com>
2011-01-21 19:34:51 +0200

    Next version (most probably wrong) of B-tree implementation

--- src/advanced-ds/btree.ml
+++ src/advanced-ds/btree.ml
@@ -2,24 +2,29 @@ open Batteries
 
 type btree =
   | Node of int list * btree list
+  | Leaf of int list
 
 let min_degree = 2
-let empty = Node ([], [])
+let max_value = max_int
+let empty = Leaf []
 
 let is_leaf = function
-  | Node (_, children) -> List.is_empty children
+  | Node _ -> false
+  | Leaf _ -> true
 
 let is_full = function
-  | Node (keys, _) -> List.length keys = 2 * min_degree - 1
+  | Node (keys, _)
+  | Leaf keys -> List.length keys = 2 * min_degree
 
 let rec find tree key = match tree with
-  | Node ([], []) -> None
+  | Leaf [] -> None
+  | Leaf keys ->
+    if List.mem key keys then Some key else None
   | Node (keys, children) ->
     if List.mem key keys then Some key
     else
       let rec find' ks cs = match ks, cs with
         | [], [] -> None
-        | [], [last] -> find last key
         | k :: ks', c :: cs' ->
           if key < k then find c key else find' ks' cs'
         | _ -> raise (Failure "Not expected to reach this clause")
@@ -30,27 +35,30 @@ let mem tree key = match find tree key with
   | None   -> false
 
 let split_full = function
-  | Node (keys, children) ->
-    let lkeys, rkeys = List.split_at (min_degree - 1) keys in
+  | Leaf keys ->
+    let lkeys, rkeys = List.split_at min_degree keys in
     let median = List.hd rkeys in
     let rkeys = List.tl rkeys in
-    match children with
-      | [] -> Node ([median], [Node (lkeys, []); Node (rkeys, [])])
-      | _ -> let i = List.length lkeys in
-             let lchildren, rchildren = List.split_at i children in
-             Node ([median], [Node (lkeys, lchildren);
-                              Node (rkeys, rchildren)])
+    Node ([median], [Leaf lkeys; Leaf rkeys])
+  | Node (keys, children) ->
+    let lkeys, rkeys = List.split_at min_degree keys in
+    let median = List.hd rkeys in
+    let rkeys = List.tl rkeys in
+    let i = List.length lkeys in
+    let lchildren, rchildren = List.split_at i children in
+    Node ([median], [Node (lkeys, lchildren);
+                     Node (rkeys, rchildren)])
 
 let split_by key keys = List.partition (fun k -> key > k) keys
 
 let rec insert tree key = match tree with
-  | Node ([], []) -> Node ([key], [])
-  | Node (keys, []) ->
+  | Leaf [] -> Leaf [key]
+  | Leaf keys ->
     if is_full tree
     then let tree = split_full tree in
          insert tree key
     else let lkeys, rkeys = split_by key keys in
-         Node (lkeys @ key :: rkeys, [])
+         Leaf (lkeys @ key :: rkeys)
   | Node (keys, children) ->
     if is_full tree
     then let tree = split_full tree in
