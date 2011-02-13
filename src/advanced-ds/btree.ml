3fbb99c949f74844995f835bf9eb6d1ac217e08e
Ivan Chernetsky <ivan.chernetsky@gmail.com>
2011-02-13 02:55:22 +0200

    Right implementation of B-tree (though, somewhat messy)

--- src/advanced-ds/btree.ml
+++ src/advanced-ds/btree.ml
@@ -1,71 +1,87 @@
 open Batteries
 
-type btree =
-  | Node of int list * btree list
+type btree = { degree : int; tree : tree }
+and tree =
+  | Node of int list * tree list
   | Leaf of int list
 
-let min_degree = 2
-let max_value = max_int
-let empty = Leaf []
+let create degree =
+  if degree < 2
+  then raise (Invalid_argument "degree < 2")
+  else { degree = degree; tree = Leaf [] }
 
-let is_leaf = function
-  | Node _ -> false
-  | Leaf _ -> true
+let find btree key =
+  let rec find' = function
+    | Leaf [] -> None
+    | Leaf keys ->
+      if List.mem key keys then Some key else None
+    | Node (keys, children) ->
+      if List.mem key keys then Some key
+      else let rec move_right ks cs =
+             match ks, cs with
+               | [], [c] -> find' c
+               | k :: ks, c :: cs ->
+                 if k > key then find' c else move_right ks cs
+               | _ -> raise (Failure "Hot we got here?!")
+           in move_right keys children
+  in find' btree.tree
 
-let is_full = function
-  | Node (keys, _)
-  | Leaf keys -> List.length keys = 2 * min_degree
+let is_full tree degree = match tree with
+  | Node (keys, _) | Leaf keys ->
+    List.length keys = 2 * degree - 1
 
-let rec find tree key = match tree with
-  | Leaf [] -> None
-  | Leaf keys ->
-    if List.mem key keys then Some key else None
+let split_at_center list =
+  let center = (List.length list) / 2 in
+  let left, right = List.split_at center list in
+  let middle, right = List.hd right, List.tl right in
+  left, middle, right
+
+let split_leaf = function
+  | Leaf keys -> let left, middle, right = split_at_center keys in
+                 Node ([middle], [Leaf left; Leaf right])
+  | _ -> raise (Invalid_argument "leaf")
+
+let split_node = function
   | Node (keys, children) ->
-    if List.mem key keys then Some key
-    else
-      let rec find' ks cs = match ks, cs with
-        | [], [] -> None
-        | k :: ks', c :: cs' ->
-          if key < k then find c key else find' ks' cs'
-        | _ -> raise (Failure "Not expected to reach this clause")
-      in find' keys children
+    let left, middle, right = split_at_center keys in
+    let i = (List.length left) + 1 in
+    let left_children, right_children = List.split_at i children in
+    Node ([middle], [Node (left,  left_children);
+                     Node (right, right_children)])
+  | _ -> raise (Invalid_argument "node")
 
-let mem tree key = match find tree key with
-  | Some _ -> true
-  | None   -> false
+let split_full tree =
+  match tree with
+    | Leaf _ -> split_leaf tree
+    | Node _ -> split_node tree
 
-let split_full = function
-  | Leaf keys ->
-    let lkeys, rkeys = List.split_at min_degree keys in
-    let median = List.hd rkeys in
-    let rkeys = List.tl rkeys in
-    Node ([median], [Leaf lkeys; Leaf rkeys])
-  | Node (keys, children) ->
-    let lkeys, rkeys = List.split_at min_degree keys in
-    let median = List.hd rkeys in
-    let rkeys = List.tl rkeys in
-    let i = List.length lkeys in
-    let lchildren, rchildren = List.split_at i children in
-    Node ([median], [Node (lkeys, lchildren);
-                     Node (rkeys, rchildren)])
+let split_with key = List.partition (fun k -> key > k)
 
-let split_by key keys = List.partition (fun k -> key > k) keys
-
-let rec insert tree key = match tree with
-  | Leaf [] -> Leaf [key]
-  | Leaf keys ->
-    if is_full tree
-    then let tree = split_full tree in
-         insert tree key
-    else let lkeys, rkeys = split_by key keys in
-         Leaf (lkeys @ key :: rkeys)
-  | Node (keys, children) ->
-    if is_full tree
-    then let tree = split_full tree in
-         insert tree key
-    else let lkeys, rkeys = split_by key keys in
-         let i = List.length lkeys in
-         let lchildren, rchildren = List.split_at i children in
-         let next = List.hd rchildren in
-         let rchildren = List.tl rchildren in
-         Node (keys, lchildren @ (insert next key) :: rchildren)
+let rec insert btree key =
+  let rec insert' tree =
+    match tree with
+      | Leaf [] -> Leaf [key]
+      | Leaf keys ->
+        if is_full tree btree.degree
+        then let tree = split_full tree in insert' tree
+        else let left, right = split_with key keys in
+             Leaf (left @ key :: right)
+      | Node (keys, children) ->
+        if is_full tree btree.degree
+        then let tree = split_full tree in insert' tree
+        else let left_keys, right_keys = split_with key keys in
+             let i = List.length left_keys in
+             let left_children, right_children = List.split_at i children in
+             let next = List.hd right_children in
+             let right_children = List.tl right_children in
+             if is_full next btree.degree
+             then match split_full next with
+               | Node ([median], [left; right]) ->
+                 let left, right =
+                   if key <= median then (insert' left, right)
+                   else (left, insert' right) in
+                 Node (left_keys @ median :: right_keys,
+                       left_children @ left :: right :: right_children)
+               | _ -> raise (Failure "How we got here?!")
+             else Node (keys, left_children @ (insert' next) :: right_children)
+  in { btree with tree = insert' btree.tree }
