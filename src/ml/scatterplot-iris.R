1e8ecb5d106c6542548a29c4af931dc6ee84ed9a
Ivan Chernetsky <ivan.chernetsky@gmail.com>
2011-03-12 16:27:52 +0200

    ML: colors of k-means output adjusted

--- src/ml/scatterplot-iris.R
+++ src/ml/scatterplot-iris.R
@@ -8,5 +8,5 @@ dev.off()
 iris <- read.csv(file = "iris-flower-kmeans.csv", head = TRUE, sep = ",")
 postscript(file = "../../images/ml/iris-flower-kmeans.ps", height = 6, width = 6,
            paper = "special", horizontal = FALSE)
-pairs(iris[1:4], pch = 21, bg = c("red", "green3", "blue")[unclass(iris$species)])
+pairs(iris[1:4], pch = 21, bg = c("green3", "red", "blue")[unclass(iris$species)])
 dev.off()
