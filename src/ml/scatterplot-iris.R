03332abfa6eed834554fe6ec5db3ca330f866185
Ivan Chernetsky <ivan.chernetsky@gmail.com>
2011-03-12 15:57:45 +0200

    ML: R code for visualization of output of k-means

--- src/ml/scatterplot-iris.R
+++ src/ml/scatterplot-iris.R
@@ -4,3 +4,9 @@ postscript(file = "../../images/ml/iris-flower-dataset.ps", height = 6, width = 
            paper = "special", horizontal = FALSE)
 pairs(iris[1:4], pch = 21, bg = c("red", "green3", "blue")[unclass(iris$species)])
 dev.off()
+
+iris <- read.csv(file = "iris-flower-kmeans.csv", head = TRUE, sep = ",")
+postscript(file = "../../images/ml/iris-flower-kmeans.ps", height = 6, width = 6,
+           paper = "special", horizontal = FALSE)
+pairs(iris[1:4], pch = 21, bg = c("red", "green3", "blue")[unclass(iris$species)])
+dev.off()
