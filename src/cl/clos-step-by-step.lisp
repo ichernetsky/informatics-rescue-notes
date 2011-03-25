1524a3ad10399ff541a6782f2d5807c0d0f3c3a6
Ivan Chernetsky <ivan.chernetsky@gmail.com>
2011-03-25 16:00:19 +0200

    CL: CLOS example code, step-by-step demonstration

---
+++ src/cl/clos-step-by-step.lisp
@@ -0,0 +1,148 @@
+(defclass bank-account () ())
+
+(defclass bank-account ()
+  (customer-name
+   balance))
+
+(defparameter *account* (make-instance 'bank-account))
+
+(setf (slot-value *account* 'customer-name) "John Doe")
+(setf (slot-value *account* 'balance) 1000)
+
+(slot-value *account* 'customer-name)
+(slot-value *account* 'balance)
+
+(defclass bank-account ()
+  ((customer-name :initarg :customer-name)
+   (balance :initarg :balance
+            :initform 0)))
+
+(defparameter *account*
+  (make-instance 'bank-account
+                 :customer-name "John Doe"
+                 :balance 1000))
+
+(slot-value (make-instance 'bank-account) 'balance)
+(slot-value (make-instance 'bank-account) 'customer-name)
+
+(defvar *account-numbers* 0)
+
+(defclass bank-account ()
+  ((customer-name :initarg :customer-name
+                  :initform (error "Must supply a customer name."))
+   (balance :initarg :balance
+            :initform 0)
+   (account-number :initform (incf *account-numbers*))))
+
+(defclass bank-account ()
+  ((customer-name
+    :initarg :customer-name
+    :initform (error "Must supply a customer name.")
+    :accessor customer-name
+    :documentation "Customer's name")
+   (balance
+    :initarg :balance
+    :initform 0
+    :reader balance
+    :documentation "Current account balance")
+   (account-number
+    :initform (incf *account-numbers*)
+    :reader account-number
+    :documentation "Account number, unique within a bank.")))
+
+(defgeneric withdraw (account amount)
+  (:documentation "Withdraw the specified amount from the account.
+Signal an error if the current balance is less than amount."))
+
+(defmethod withdraw ((account bank-account) amount)
+  (when (< (balance account) amount)
+    (error "Account overdrawn."))
+  (decf (slot-value account 'balance) amount))
+
+(defclass checking-account (bank-account)
+  ((overdraft-account
+    :initarg :overdraft-account
+    :initform (error "Must supply an overdraft account.")
+    :reader overdraft-account)))
+
+(defmethod withdraw ((account checking-account) amount)
+  (let ((overdraft (- amount (balance account))))
+    (when (plusp overdraft)
+      (withdraw (overdraft-account account) overdraft)
+      (incf (slot-value account 'balance) overdraft)))
+  (call-next-method))
+
+(defvar *account-of-bank-president*
+  (make-instance 'bank-account
+                 :customer-name "Mr. President"
+                 :balance 100000000000))
+
+(defclass bank () ())
+(defvar *bank* (make-instance 'bank))
+
+(defun embezzle (bank overdraft)
+  (declare (ignore bank))
+  overdraft)
+
+(defmethod withdraw ((account (eql *account-of-bank-president*)) amount)
+  (let ((overdraft (- amount (balance account))))
+    (when (plusp overdraft)
+      (incf (slot-value account 'balance) (embezzle *bank* overdraft)))
+    (call-next-method)))
+
+(defmethod withdraw :before ((account checking-account) amount)
+  (let ((overdraft (- amount (balance account))))
+    (when (plusp overdraft)
+      (withdraw (overdraft-account account) overdraft)
+      (incf (slot-value account 'balance) overdraft))))
+
+(defclass bank-account ()
+  ((customer-name
+    :initarg :customer-name
+    :initform (error "Must supply a customer name.")
+    :accessor customer-name
+    :documentation "Customer's name")
+   (balance
+    :initarg :balance
+    :initform 0
+    :reader balance
+    :documentation "Current account balance")
+   (account-number
+    :initform (incf *account-numbers*)
+    :reader account-number
+    :documentation "Account number, unique within a bank.")
+   (account-type
+    :reader account-type
+    :documentation "Type of account, one of :gold, :silver, or :bronze.")))
+
+(defmethod initialize-instance :after ((account bank-account) &key)
+  (let ((balance (slot-value account 'balance)))
+    (setf (slot-value account 'account-type)
+          (cond
+            ((>= balance 100000) :gold)
+            ((>= balance 50000) :silver)
+            (t :bronze)))))
+
+(defclass job () ())
+(defclass express-job (job) ())
+
+(defgeneric priority (job)
+  (:documentation "Return the priority at which the job should be run.")
+  (:method-combination +))
+
+(defgeneric priority (job)
+  (:documentation "Return the priority at which the job should be run.")
+  (:method-combination + :most-specific-last))
+
+(defmethod priority + ((job express-job)) 10)
+
+(defgeneric beat (drum stick)
+  (:documentation
+   "Produce a sound by hitting the given drum with the given stick."))
+
+(defmethod beat ((drum snare-drum) (stick wooden-drumstick)) ...)
+(defmethod beat ((drum snare-drum) (stick brush)) ...)
+(defmethod beat ((drum snare-drum) (stick soft-mallet)) ...)
+(defmethod beat ((drum tom-tom) (stick wooden-drumstick)) ...)
+(defmethod beat ((drum tom-tom) (stick brush)) ...)
+(defmethod beat ((drum tom-tom) (stick soft-mallet)) ...)
