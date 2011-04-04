06e59c18e8d42d7b425b0721e499a4f27719cf6b
Ivan Chernetsky <ivan.chernetsky@gmail.com>
2011-04-04 17:29:00 +0300

    ML: description of kd-tree and implementation of its construction

--- src/ml/knn.py
+++ src/ml/knn.py
@@ -1,6 +1,8 @@
 import csv
 import metrics
 
+from numpy import *
+
 def knn(item, dataset):
     klass = None
     nearest = None
@@ -16,6 +18,29 @@ def knn(item, dataset):
 
     return klass
 
+class Node(object):
+    def __init__(self, location, left, right):
+        self.location = location
+        self.left = left
+        self.right = right
+
+def kdtree(points, depth=0):
+    if not points.size:
+        return None
+
+    n, k = points.shape
+    axis = depth % k
+
+    points.sort(axis=axis)
+    median = n // 2
+
+    location = points[median]
+    left = kdtree(points[:median], depth + 1)
+    right = kdtree(points[median + 1:], depth + 1)
+    node = Node(location, left, right)
+
+    return node
+
 if __name__ == "__main__":
     reader = csv.reader(open('iris-flower-dataset.csv', 'r'),
                         quoting=csv.QUOTE_NONNUMERIC)
@@ -27,3 +52,9 @@ if __name__ == "__main__":
                          "class"    : row[4]   })
 
     print knn([4.5, 3.8, 1.2, 0.4], dataset)
+    print kdtree(array([(2,3),
+                        (5,4),
+                        (9,6),
+                        (4,7),
+                        (8,1),
+                        (7,2)]))
