dea54d8286f5c3c386bede3c28a90569db3c5e16
Ivan Chernetsky <ivan.chernetsky@gmail.com>
2011-11-10 09:59:53 +0300

    Polished implementation of quicksort

--- src/basic-ds/qsort.c
+++ src/basic-ds/qsort.c
@@ -1,43 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ARRAY_LENGTH(a) (sizeof (a) / sizeof (a[0]))
+#define array_length(a)                         \
+    (sizeof(a) / sizeof((a)[0]))                \
 
-void swap (int [], int, int);
-void quicksort (int [], int);
-
-int main (void) {
-  int i;
-  int a[] = { 7, 5, 1, 7, 5, 9, 0, 3, 6 };
-  int length = ARRAY_LENGTH (a);
-
-  quicksort (a, length);
-  for (i = 0; i < length; i++)
-    printf ("%d ", a[i]);
-
-  return EXIT_SUCCESS;
+static inline void swap(int a[], int i, int j)
+{
+    int tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
 }
 
-void swap (int a[], int i, int j) {
-  int tmp;
+void quicksort(int a[], int n)
+{
+    int i, last = 0;
 
-  tmp = a[i];
-  a[i] = a[j];
-  a[j] = tmp;
+    if (n <= 1)
+        return;
+
+    swap (a, 0, rand () % n);
+    for (i = 1; i < n; i++)
+        if (a[i] < a[0])
+            swap(a, ++last, i);
+    swap(a, 0, last);
+
+    quicksort(a, last);
+    quicksort(a + last + 1, n - last - 1);
 }
 
-void quicksort (int a[], int n) {
-  int i, last;
-  if (n <= 1)
-    return;
+int main(void)
+{
+    int a[] = { 7, 5, 1, 7, 5, 9, 0, 3, 6 };
+    int length = array_length(a);
 
-  swap (a, 0, rand () % n);
-  last = 0;
-  for (i = 1; i < n; i++)
-    if (a[i] < a[0])
-      swap (a, ++last, i);
-  swap (a, 0, last);
+    quicksort(a, length);
+    for (int i = 0; i < length; i++)
+        printf("%d ", a[i]);
 
-  quicksort (a, last);
-  quicksort (a + last + 1, n - last - 1);
+    return 0;
 }
