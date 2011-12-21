b78c0815e9a48cb4b825b4186e92c90b6fc9afd9
Ivan Chernetsky <ivan.chernetsky@gmail.com>
2011-12-22 02:38:08 +0300

    Python: changes to wiki-gallery.py
     * Configuring logging and using it through module functions
     * Changed a bit interface and control flows of Worker and WorkerPool
     * Scraping and writing metadata to images
     * Downloading next pages of category

--- src/python/wiki-gallery.py
+++ src/python/wiki-gallery.py
@@ -6,49 +6,62 @@ import logging
 import lxml.etree
 import os.path
 import Queue
+import subprocess
 import threading
 import urllib
 import urllib2
 import urlparse
 
-class Thread(threading.Thread):
-    def __init__(self, tasks):
-        super(Thread, self).__init__()
-        self.tasks = tasks
+class Worker(threading.Thread):
+    def __init__(self, pool):
+        super(Worker, self).__init__()
+
+        self.pool   = pool
         self.daemon = True
 
     def run(self):
-        logger.debug('%s has been started', self.name)
+        logging.debug('%s has been started', self.name)
         while True:
-            fn = self.tasks.get()
+            fn = self.pool.next_task()
             try:
-                fn()
+                fn(self.pool)
             except Exception, e:
-                logger.debug('%s got exception', self.name, exc_info=e)
-            self.tasks.task_done()
+                logging.error('%s got exception', self.name, exc_info=e)
+            self.pool.task_done()
 
-class ThreadPool(object):
-    def __init__(self, num_threads):
-        logger.debug('Initializing ThreadPool with %u workers', num_threads)
-        self.tasks = Queue.Queue()
-        self.threads = [Thread(self.tasks) for i in xrange(num_threads)]
-        logger.debug('ThreadPool has been initialized')
+class WorkerPool(object):
+    def __init__(self, num_workers):
+        logging.debug('Initializing WorkerPool with %u workers', num_workers)
+
+        self.tasks   = Queue.Queue()
+        self.workers = [Worker(self) for i in xrange(num_workers)]
+
+        logging.debug('WorkerPool has been initialized')
 
     def start(self):
-        for thread in self.threads:
-            thread.start()
+        for worker in self.workers:
+            worker.start()
+        logging.debug('All workers have been started')
 
     def add(self, task):
         self.tasks.put(task)
-        logger.debug('%s has been put in queue', task)
+
+    def next_task(self):
+        return self.tasks.get()
+
+    def task_done(self):
+        self.tasks.task_done()
 
     def join(self):
         self.tasks.join()
-        logger.debug('All threads are stopped')
+        logging.debug('All workers are stopped')
 
 class UrlHandler(object):
     def __init__(self, url):
         self.url = url
+
+    def __repr__(self):
+        return self.__str__()
 
     def __str__(self):
         return '%s for %s' % (self.__class__.__name__, self.url)
@@ -56,6 +69,8 @@ class UrlHandler(object):
     @staticmethod
     def download(url):
         request = urllib2.Request(url=url)
+        request.add_header('Accept-Charset', 'UTF-8,*;q=0.5')
+        request.add_header('Accept-Language', 'en-US,en;q=0.8')
         request.add_header('User-agent', 'Mozilla/5.0')
         return urllib2.urlopen(request)
 
@@ -66,42 +81,79 @@ class UrlHandler(object):
         return tree
 
 class CategoryHandler(UrlHandler):
-    def __init__(self, url, pool):
+    def __init__(self, url):
         super(CategoryHandler, self).__init__(url)
-        self.pool = pool
 
-    def __call__(self):
+    def __call__(self, pool):
         page = self.download(self.url)
 
-        file_pages = self.parse(page)
+        categories, file_pages = self.parse(page)
+
+        for u in categories:
+            handler = CategoryHandler(u)
+            pool.add(handler)
+
         for u in file_pages:
             handler = FileHandler(u)
-            self.pool.add(handler)
+            pool.add(handler)
 
     def parse(self, page):
         tree = self.make_tree(page)
-        return [urlparse.urljoin(self.url, url)
-                for url in tree.xpath('//li[@class="gallerybox"]//a[@class="image"]/@href')]
+
+        categories = tree.xpath('//div[@id="mw-category-media"]/a')
+        categories = [c.attrib['href'] for c in categories
+                      if unicode(c.text).strip().lower() == u'next 200']
+        categories = [urlparse.urljoin(self.url, c)
+                      for c in categories]
+        categories = list(set(categories))
+
+        file_pages = [urlparse.urljoin(self.url, url)
+                      for url in tree.xpath('//li[@class="gallerybox"]//a[@class="image"]/@href')]
+
+        return categories, file_pages
 
 class FileHandler(UrlHandler):
-    def __call__(self):
+    def __call__(self, pool):
         page = self.download(self.url)
 
-        files = self.parse(page)
+        description, files = self.parse(page)
         for u in files:
             fname = self.file_name(u)
             if os.path.exists(fname):
+                logging.warning('%s exists, skipping it', fname)
                 continue
 
             remotef = self.download(u)
-            f = file(fname, "w")
-            f.write(remotef.read())
-            logger.info('%s downloaded', u)
+            localf = file(fname, "w")
+            localf.write(remotef.read())
+
+            if description:
+                try:
+                    ret = subprocess.call([u'exiv2',
+                                           u'-Mset Exif.Photo.UserComment %s' % description,
+                                           fname])
+                    logging.debug('exiv2 returned %u', ret)
+                except:
+                    pass
+
+            logging.info('File %s downloaded', u)
 
     def parse(self, page):
         tree = self.make_tree(page)
-        return [urlparse.urljoin(self.url, url)
-                for url in tree.xpath('//div[@class="fullImageLink"]/a/@href')]
+
+        description = list()
+        for text in tree.xpath('//span[@id="creator"]/a/text()'):
+            description.append(text)
+        for elem in tree.xpath('//div[contains(@class,"description")]'):
+            description.append(u' '.join(e.text for e in elem.iter()
+                                         if e.text))
+
+        description = u' — '.join(d.strip() for d in description)
+
+        files = [urlparse.urljoin(self.url, url)
+                 for url in tree.xpath('//div[@class="fullImageLink"]/a/@href')]
+
+        return description, files
 
     @staticmethod
     def file_name(url):
@@ -109,17 +161,10 @@ class FileHandler(UrlHandler):
         fname = urllib.unquote_plus(fname)
         return fname
 
-logger = None
-
-def setup_logger(level):
-    global logger
-    logger = logging.getLogger('wiki-gallery')
-    logger.setLevel(getattr(logging, level.upper()))
-    console = logging.StreamHandler()
-    console.setLevel(getattr(logging, level.upper()))
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    console.setFormatter(formatter)
-    logger.addHandler(console)
+def setup_logging(level):
+    level = getattr(logging, level.upper())
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=level)
 
 def main():
     parser = argparse.ArgumentParser(description='Download images from wiki categories.')
@@ -130,12 +175,12 @@ def main():
                         default='warning', help='log level')
     args = parser.parse_args()
 
-    setup_logger(args.log_level)
+    setup_logging(args.log_level)
 
-    pool = ThreadPool(20)
+    pool = WorkerPool(20)
 
     for url in args.urls:
-        page_handler = CategoryHandler(url, pool)
+        page_handler = CategoryHandler(url)
         pool.add(page_handler)
 
     pool.start()
